deb17e52bab9449262b3bfc123b3e5c5a1b63414
osborn roxas <osbornroxas02@gmail.com>
2019-04-21 16:03:06 +0800

    percentage chart

--- js/script.js
+++ js/script.js
@@ -19,25 +19,41 @@ $(document).ready(function() {
 	/*-- SKILLS SECTION --*/
 	$('.owl-carousel').owlCarousel({
 	    loop:true,
-	    margin:10,
-	    nav:true,
+	    items: 4,
 	    responsive:{
 	        0:{
 	            items:1
 	        },
-	        600:{
+	        480:{
+	            items:2
+	        },
+	        768:{
 	            items:3
 	        },
-	        1000:{
-	            items:5
+	        938:{
+	            items:4
 	        }
 	    }
 	});
 
 	$('.chart').easyPieChart({
-         //your options goes here
+         easing: 'easeInOut',
+         barColor: '#fff',
+         trackColor: '#74b9ff', /*instead of false*/
+         scaelColor: 'false',
+         lineWidth: 4,
+         size: 152,
+         onStep: function(from, to, percent) {
+         	$(this.el).find('.percent').text(Math.round(percent));
+         }
     });
 
 });/*-- END SKILLS SECTION --*/
+
+
+
+
+
+
 
 
