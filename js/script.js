07623d5180e7a740fd2a81486c9009d85ee23c18
osborn roxas <osbornroxas02@gmail.com>
2019-04-23 08:13:27 +0800

    end skill set section

--- js/script.js
+++ js/script.js
@@ -36,17 +36,26 @@ $(document).ready(function() {
 	    }
 	});
 
-	$('.chart').easyPieChart({
-         easing: 'easeInOut',
-         barColor: '#fff',
-         trackColor: '#74b9ff', /*instead of false*/
-         scaelColor: 'false',
-         lineWidth: 4,
-         size: 152,
-         onStep: function(from, to, percent) {
-         	$(this.el).find('.percent').text(Math.round(percent));
-         }
-    });
+	var skillsTopOffset = $(".skillsSection").offset().top;
+
+	$(window).scroll(function() {
+
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		         easing: 'easeInOut',
+		         barColor: '#fff',
+		         trackColor: '#74b9ff', /*instead of false*/
+		         scaelColor: 'false',
+		         lineWidth: 4,
+		         size: 152,
+		         onStep: function(from, to, percent) {
+	         		$(this.el).find('.percent').text(Math.round(percent));
+	         	}
+   		 	});
+		}
+
+	}); 
 
 });/*-- END SKILLS SECTION --*/
 
