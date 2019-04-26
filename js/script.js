730e16c4134108fa564f062c616b181ee2a6e460
osborn roxas <osbornroxas02@gmail.com>
2019-04-26 02:10:27 -0700

    portfolio icon filtering

--- js/script.js
+++ js/script.js
@@ -85,6 +85,27 @@ $(document).ready(function() {
 		}
 	})
 
+	$("#filters a").click(function() {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+
+		return false;
+
+	})
+
+
 });
 
 
